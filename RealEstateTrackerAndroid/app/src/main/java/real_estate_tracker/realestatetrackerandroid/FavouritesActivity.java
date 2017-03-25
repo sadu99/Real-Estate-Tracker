4f515f140dd68fe290001a140439ebde23edcfb6
Sanhar Balachandran <s6balach@uwaterloo.ca>
2017-03-24 23:08:01 -0400

    fabourites dn toolbar

---
+++ RealEstateTrackerAndroid/app/src/main/java/real_estate_tracker/realestatetrackerandroid/FavouritesActivity.java
@@ -0,0 +1,74 @@
+package real_estate_tracker.realestatetrackerandroid;
+
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FavouritesActivity extends NavigationActivity{
+
+    private List<PropertyObject> mPropertiesList = new ArrayList<>();
+    private RecyclerView mRecyclerView;
+    private PropertyAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FrameLayout contentFrameLayout = (FrameLayout) findViewById(R.id.content_frame); //Remember this is the FrameLayout area within your activity_main.xml
+        getLayoutInflater().inflate(R.layout.activity_favourites, contentFrameLayout);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        mAdapter = new PropertyAdapter(mPropertiesList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        mRecyclerView.setAdapter(mAdapter);
+
+        mRecyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), mRecyclerView, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                PropertyObject propertyObject = mPropertiesList.get(position);
+                Toast.makeText(getApplicationContext(), propertyObject.getArea() + " is selected!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+
+        preparePropertyData();
+    }
+
+    private void preparePropertyData() {
+        PropertyObject propertyObject = new PropertyObject("asas", "asasa",new LatLng(51.5074,-0.1278),"asasas");
+        mPropertiesList.add(propertyObject);
+        propertyObject = new PropertyObject("asas", "asasa",new LatLng(51.6074,-0.1278),"asasas");
+        mPropertiesList.add(propertyObject);
+        propertyObject = new PropertyObject("asas", "asasa",new LatLng(51.6074,-0.1478),"asasas");
+        mPropertiesList.add(propertyObject);
+        mAdapter.notifyDataSetChanged();
+    }
+}
---
+++ RealEstateTrackerAndroid/app/src/main/java/real_estate_tracker/realestatetrackerandroid/NavigationActivity.java
@@ -0,0 +1,84 @@
+package real_estate_tracker.realestatetrackerandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+/**
+ * Created by Sanhar on 2017-03-24.
+ */
+
+public class NavigationActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private ActionBarDrawerToggle mToggle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(mToggle);
+        mToggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_search) {
+            // Handle the search activity
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }else if (id == R.id.nav_favourite) {
+            // Handle the favourites activity
+            Intent intent = new Intent(this, FavouritesActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_settings) {
+            // Handle the settings activity
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//
+//        if (mToggle.onOptionsItemSelected(item)) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//
+//    }
+//
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mToggle.syncState();
+    }
+
+}
